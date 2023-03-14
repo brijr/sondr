33fb2504401c058fd674701ad69a03bf671ce77a
Bridger Tower <bridger@ziondesign.io>
2023-03-14 14:39:19 -0600

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,24 @@ import './globals.css';
 export const metadata = {
 	title: 'Sondr | Digital Marketing for Healthcare',
 	description:
-		'Sondr is a digital marketing agency that helps healthcare organizations grow their business online.'
+		'Sondr is a digital marketing agency that helps healthcare organizations grow their business online.',
+	keywords: ['Digital Marketing', 'Assisted Living', 'Hospice', 'Home Care'],
+	openGraph: {
+		title: 'Sondr | Digital Marketing for Healthcare',
+		description:
+			'Sondr is a digital marketing agency that helps healthcare organizations grow their business online.',
+		url: 'https://sondrmarketing.com',
+		siteName: 'Sondr | Digital Marketing for Healthcare',
+		images: [
+			{
+				url: '/og.jpg',
+				width: 629,
+				height: 324
+			}
+		],
+		locale: 'en-US',
+		type: 'website'
+	}
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
